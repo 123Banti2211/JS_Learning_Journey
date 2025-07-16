87323127b52544bbce564977f321aaa28a7b401c
123Banti2211 <bantijoarder2211@gmail.com>
2025-07-17 01:43:12 +0530

    Basics

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,40 @@
+// Singletone
+//Object.create
+
+// Object literals
+
+const mySym = Symbol ("Key1")
+
+const JsUser = {
+    name : "Banti",
+    "full name" : "Banti Joarder",
+    [mySym] : "myKey1",
+    age : 18,
+    location:  "jaipur",
+    email : "banti@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday","Saturday"]
+}
+/*console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+console.log( JsUser[mySym]);*/
+
+JsUser.email = "banti@google.com"
+//Object.freeze(JsUser)
+JsUser.email = "banti@g2.com"
+//console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hello JS user");
+    
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`hello JS user", ${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,58 @@
+//let a = 10
+//const b = 20
+//var c = 300
+let a = 100000
+
+if(true){
+    let a = 10
+    const b = 20
+    //var c = 30
+    d = 40
+    //console.log("Inner: ",a);
+}
+
+//console.log(a)
+//console.log(b)
+//console.log(c);
+//console.log(d);
+
+function one()
+{
+    const username = "banti"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+if (true) {
+    const username = "banti"
+    if (username === "banti") {
+        const website = "youtube"
+        //console.log(username + website);
+        
+        
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+// +++++++++++++++++++++++++   interesting   +++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num+1
+}
+
+//console.log(addTwo(5));
+
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(5));
